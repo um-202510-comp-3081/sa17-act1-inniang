eaa32b91b5b23a6b3a0dbf2d6aff3af22c34acc3
Ibu <iniang1@southwest.tn.edu>
2025-04-17 02:56:49 -0500

    Update board_games_controller.rb

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -10,7 +10,7 @@ class BoardGamesController < ApplicationController
     
         unless @board_game
           flash[:alert] = "Board game not found."
-          redirect_to board_games_path  # Redirect to index if the game doesn't exist
+          redirect_to board_games_path  
         end
       end
 end
